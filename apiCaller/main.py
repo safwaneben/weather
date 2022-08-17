98c61b908e527217dc359c8b29be3015cd5be238
safwaneben <benidirsafwane@gmail.com>
2022-08-17 21:16:58 +0200

    add curl in container and retrieve data from weather api

--- apiCaller/main.py
+++ apiCaller/main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p><strong>Hello, World!</strong></p>"
+@app.route("/get_data")
+def get_data():
+    params = {
+    'access_key': '0d04d9264a63b76659c58596c16e0fda',
+    'query': 'Paris'
+    }
+    api_result = requests.get('http://api.weatherstack.com/current', params)
+    api_response = api_result.json()
+    return api_response['location']['name'], api_response['current']['temperature']
 
 if __name__ == '__main__':
 
