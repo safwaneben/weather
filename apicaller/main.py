14e881aacc0fe4e0dcc31a3a2fbd38a5a9bb3a07
safwaneben <benidirsafwane@gmail.com>
2022-08-18 23:55:17 +0200

    add Dockerfile and docker compose + open container in localhost

--- apicaller/main.py
+++ apicaller/main.py
@@ -4,8 +4,8 @@ import requests
 app = Flask(__name__)
 
 
-@app.route("/get_data")
-def get_data():
+@app.route("/apicaller")
+def apicaller():
     params = {
     'access_key': '0d04d9264a63b76659c58596c16e0fda',
     'query': 'Paris'
@@ -17,6 +17,11 @@ def get_data():
         "Temperature":api_response['current']['temperature']
     }
 
+HOST = '0.0.0.0'
+PORT = 8081
+
 if __name__ == '__main__':
 
-    app.run()
+    app.run(host=HOST,
+            debug=False,  # automatic reloading enabled
+            port=PORT)
---
+++ dataparser/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/data_parser")
+def data_parser():
+    return "<p><strong>Je suis le data parser</strong></p>"
+
+
+HOST = '0.0.0.0'
+PORT = 8082
+
+
+if __name__ == '__main__':
+
+    app.run(host=HOST,
+            debug=False,  # automatic reloading enabled
+            port=PORT)
