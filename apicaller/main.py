93407a0617aa386a39956f3e2e9b3aca756ff7d2
Anas <anas.francis@etu.univ-cotedazur.fr>
2022-09-17 10:06:04 +0200

    setup database

--- apicaller/main.py
+++ apicaller/main.py
@@ -1,15 +1,26 @@
 from flask import Flask
 import requests
 import json
+from flask_pymongo import PyMongo
+
+
+#client = MongoClient('mongo', 27017, username='root', password='example')
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://root:example@mongo:27017/"
+mongo = PyMongo(app)
+
 @app.route("/apicaller")
 def apicaller():
+
+    print('mongo = ', mongo)
+
     params = {
     'access_key': '0d04d9264a63b76659c58596c16e0fda',
     'query': 'Paris'
     }
+
     api_result = requests.get('http://api.weatherstack.com/current', params)
     api_response = api_result.json()
     res = {
@@ -21,7 +32,7 @@ def apicaller():
 
 
 HOST = '0.0.0.0'
-PORT = 8081
+PORT = 8080
 
 if __name__ == '__main__':
 
