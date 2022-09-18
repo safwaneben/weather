bf48c21493340358489976aef2d7d0ff8ae81874
Anas <anas.francis@etu.univ-cotedazur.fr>
2022-09-18 18:20:10 +0200

    mongodb not working

--- apicaller/main.py
+++ apicaller/main.py
@@ -1,20 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
-import json
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 
 #client = MongoClient('mongo', 27017, username='root', password='example')
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://root:example@mongo:27017/"
-mongo = PyMongo(app)
+
+def get_db():
+    print('Getting DB ...')
+    client = MongoClient(host='test_mongodb',
+                         port=27017, 
+                         username='root', 
+                         password='example',
+                        authSource="admin")
+    print('client = ', client)
+    db = client["weather_db"]
+    return db
+
+@app.route('/')
+def ping_server():
+    return "Welcome to the world of animals."
+
+@app.route('/animals')
+def get_stored_animals():
+    db=""
+    try:
+        db = get_db()
+        print('db = ', db)
+        _animals = db.animal_tb.find()
+        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+        return jsonify({"animals": animals})
+
+    except:
+        pass
+    finally:
+        if type(db)==MongoClient:
+            db.close()
+
 
 @app.route("/apicaller")
 def apicaller():
 
-    print('mongo = ', mongo)
 
     params = {
     'access_key': '0d04d9264a63b76659c58596c16e0fda',
