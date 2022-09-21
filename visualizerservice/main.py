75792229e5aac3559f287a775f2b7689cc41224b
safwaneben <benidirsafwane@gmail.com>
2022-09-21 23:07:50 +0200

    add plotly graph + communication with prediction service

--- visualizerservice/main.py
+++ visualizerservice/main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
+import plotly.graph_objects as go
+import plotly
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/visualizer")
 def visualizer():
-    return "<p><strong>Je suis le visualizer</strong></p>"
+    FORECAST_HORIZON=10
+    response = requests.get('http://172.17.0.3:8083/prediction')
+
+    fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+    fig.add_trace(go.Scatter(
+        x=[i for i in range(FORECAST_HORIZON)],
+        y=response.json()["predictions"],
+        name="Predictions"
+    ))
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    header="Predictions"
+    return render_template('pred.html', graphJSON=graphJSON, header=header)
 
 
 HOST = '0.0.0.0'
@@ -16,5 +31,5 @@ PORT = 8084
 if __name__ == '__main__':
 
     app.run(host=HOST,
-            debug=False,  # automatic reloading enabled
+            debug=True,  # automatic reloading enabled
             port=PORT)
