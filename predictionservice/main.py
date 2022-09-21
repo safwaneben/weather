eefabee6d1735c50fe9c8c1df84a33f21a054810
safwaneben <benidirsafwane@gmail.com>
2022-09-21 23:07:05 +0200

    add holt winters models to predict but with random data

--- predictionservice/main.py
+++ predictionservice/main.py
@@ -1,12 +1,30 @@
 from flask import Flask
 import requests
 
+import holt_winters
+import numpy as np
+
 app = Flask(__name__)
 
+N_DATAS = 100
+FORECAST_HORIZON = 10
+
+def init_data():
+    df = []
+    for i in range(N_DATAS):
+        df.append(np.random.randint(10,30))
+    return df
+
+def predict(df):
+    model = holt_winters.HoltWinters()
+    model.fit(df)
+    return model.predict(FORECAST_HORIZON)[-FORECAST_HORIZON:]
 
 @app.route("/prediction")
 def prediction():
-    return "<p><strong>Je suis le prediction service</strong></p>"
+    df = init_data()
+    predictions = predict(df)
+    return {"predictions" : predictions}
 
 HOST = '0.0.0.0'
 PORT = 8083
@@ -14,5 +32,4 @@ PORT = 8083
 if __name__ == '__main__':
 
     app.run(host=HOST,
-            debug=False,  # automatic reloading enabled
             port=PORT)
